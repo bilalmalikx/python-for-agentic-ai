223e4fecfa00d651d3e44b5c9691674de04fdf32
Bilal Siddique Malik <outlookbilalmalik@gmail.com>
2025-07-21 00:28:15 +0500

    Initial commit

---
+++ variables in python/variables.py
@@ -0,0 +1,21 @@
+# Introduction to Variables in Python
+
+# A variable is a name that refers to a value. You can use variables to store data.
+
+# Example: Assigning values to variables
+name: str = "bilal"  # String variable
+age: int = 18  # Integer variable
+hight: float = 1.89  # Float variable
+is_Student: bool = True  # Boolean variable
+
+# You can print the values of variables
+print("name:", name)
+print("age:", age)
+print("height:", hight)
+print("am i student:", is_Student)
+
+# Variables can be changed (reassigned)
+name:str = "bilal malik"
+print("Updates name:", name)
+
+# Python is dynamically typed, so you don't need to declare the type of a variable.
